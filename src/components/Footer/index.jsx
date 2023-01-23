d1fe0e7149245a85c8aad10cd793e813c5306104
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-01-23 16:47:04 -0300

    feat: creating the footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import style from '@/styles/Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            <p>Desenvolvido por Alura.</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/Footer"
 import HeaderBar from "./components/Header"
 import Home from "./pages/Home"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-        <HeaderBar />
-            <Routes>
-               <Route path="/" exact element={<Home />} />
-               <Route path="/*" element={<p>Page not Found</p>} />
-            </Routes>
+            <HeaderBar />
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/*" element={<p>Page not Found</p>} />
+                </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
