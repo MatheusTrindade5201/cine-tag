756dc0d9fb07a1ede4730947be87770a3c872850
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-01-26 15:54:27 -0300

    feat: externalizing the json

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,28 @@
 import Banner from "@/components/Banner/index"
 import Title from "@/components/Title/index"
 import Movies from "@/components/Movies/index"
-
-import json from '@/json/db.json'
+import { useEffect, useState } from "react"
 
 
 const Home = () => {
+
+    const [ movies, setMovies ] = useState([])
+    
+    useEffect(() => async () => {
+        try {
+            const data = await fetch('https://my-json-server.typicode.com/MatheusTrindade5201/cine-tag-json/videos');
+            const json = await data.json()
+            setMovies(json)
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [])
+
     return (
         <>
             <Banner image={'/images/Banner_Home.png'} />
             <Title>Um lugar para guardar seus vídeos e filmes!</Title>
-            <Movies MovieList={json}/>
+            <Movies MovieList={movies}/>
         </>
     )
 }
